e60d2d65889b6922f43edefed02faa68a5404c4e
Lindsey Handley <lindseyhandley@gmail.com>
2017-03-05 13:42:23 -0800

    cleacleaned up some tslint stuff

--- src/components/question-and-image.tsx
+++ src/components/question-and-image.tsx
@@ -2,29 +2,29 @@ import * as React from 'react';
 
 class QuestionAndImage extends React.Component<any,any> {
   constructor(props){
-    super(props)
+    super(props);
   }
   
   render() {
     return(
       <div style={{
-	backgroundImage: "url("+this.props.thing.img+")", 
+	backgroundImage: 'url('+this.props.thing.img+')', 
 	padding: 30, 
-	backgroundSize: "cover", 
+	backgroundSize: 'cover',
 	width: screen.width / 1.5, 
         height: 500,
-      	margin: "auto"
+      	margin: 'auto'
       }}>
         <span style={{
-	  backgroundColor: "rgba(0,0,0,0.5)", 
-	  color: this.props.thing.color || "white", 
-	  fontFamily: "courier", 
-	  fontSize: "20pt"
+	  backgroundColor: 'rgba(0,0,0,0.5)', 
+	  color: this.props.thing.color || 'white', 
+	  fontFamily: 'courier',
+	  fontSize: '20pt'
         }}>
       	  {this.props.thing.question}
         </span>
       </div>
-    )
+    );
   } 
 }
 
--- src/components/question-viewer.tsx
+++ src/components/question-viewer.tsx
@@ -4,41 +4,45 @@ import QuestionAndImage from './question-and-image';
 
 class QuestionViewer extends React.Component<any, any> {
   constructor(props){
-    super(props)
-    this.state = {}
-    this.state.current = 0
-    this.state.depth = 1
-  }
+    super(props);
+    this.state = {};
+    this.state.current = 0;
+    this.state.depth = 1;
+  };
       
   depthButton(n){
     return (
       <button style={{
-        background: this.state.depth == n ? "gray" : "white", 
-        border: "none"}} 
-	onClick={()=>{this.setDepth(n)}}>{n}
+        background: this.state.depth === n ? 'gray' : 'white', 
+        border: 'none'}} 
+	onClick={()=>{
+          this.setDepth(n);
+	}}>
+	{n}
       </button>
-    )
-  }
-  
+    );
+  };
+
+  // Return all questions in the data set where the intimacy depth is the same as what's currently saved in state 
   getData(){
     return this.props.data
       .filter((d)=>{
-        return d.depth == this.state.depth
-      })
-  }
+        return (d.depth === this.state.depth);
+      });
+  };
 
   setDepth(depth){
     this.setState({
       depth: depth, 
       current: 0
-    })
-  }
+    });
+  };
 
   next(){
     this.setState({
       current: (this.state.current + 1) % this.getData().length
-    })
-  }
+    });
+  };
 
   render() {
     return (
@@ -46,17 +50,18 @@ class QuestionViewer extends React.Component<any, any> {
 	<div onClick={this.next.bind(this)}>
 	  <QuestionAndImage thing={this.getData()[this.state.current]} />
 	</div>
-	<div style={{textAlign: "center"}}>
+	<div style={{textAlign: 'center'}}>
+	  {'Intimacy level: '}
 	  {this.depthButton(1)}
 	  {this.depthButton(2)}
 	  {this.depthButton(3)}
 	  <div>
-	    {this.getData().length + " questions."}
+	    {'There are ' + this.getData().length + ' questions at this intimacy level.'}
 	  </div>
 	</div>
       </div>
     );
   }
-}
+};
 
 export default QuestionViewer;
